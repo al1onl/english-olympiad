6abdd3a932043b5cee5a443c97e845a15a6ea930
al1nol <kirilkovalenko2010@gmail.com>
2025-10-31 01:31:18 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,19 @@
+// firebase-config.js
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAPnJFF03dqUF6hc9J4i5sL-K02LjavA5Q",
+    authDomain: "olympiad-app-2024-591ee.firebaseapp.com",
+    projectId: "olympiad-app-2024-591ee",
+    storageBucket: "olympiad-app-2024-591ee.firebasestorage.app",
+    messagingSenderId: "421657499179",
+    appId: "1:421657499179:web:95a510e9d9fc146c6dfc6e"
+};
+
+// Ініціалізація Firebase
+if (typeof firebase !== 'undefined' && !firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+
+// Експорт об'єктів для доступу в script.js
+const auth = firebase.auth();
+const db = firebase.firestore();
